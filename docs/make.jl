68efc132a48452cbb7e3525a87ebb4a70a6be575
Johann Antonio Duffek <LazyScholar@users.noreply.github.com>
2021-06-19 11:36:27 +0200

    Added sorting flags which are now supported by Bibliography.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,7 @@
 using Documenter
 using DocumenterCitations
 
-bib = CitationBibliography(joinpath(@__DIR__, "example.bib"))
+bib = CitationBibliography(joinpath(@__DIR__, "example.bib"),:nyt)
 
 makedocs(
     bib,
